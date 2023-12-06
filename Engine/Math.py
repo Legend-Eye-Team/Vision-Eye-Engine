a53bf7d269ec5750ab5d108dfca7b7dea0702f39
quangladn <matthn.team@gmail.com>
2023-12-06 17:29:37 +0700

    V0.0.1.4.1

--- Engine/Math.py
+++ Engine/Math.py
@@ -1,8 +1,8 @@
 import math
 from .Graphic import Graphic
 
-def getDistance(rect1:Graphic, rect2:Graphic):
-    dx = rect2.x+rect2.width /2 - rect1.x
-    dy = rect2.y+rect2.height/2 - rect1.y
+def getDistance(graphic1:Graphic,  graphic2:Graphic):
+    dx =  graphic2.x+ graphic2.width /2 - graphic1.x
+    dy =  graphic2.y+ graphic2.height/2 - graphic1.y
     distance = math.sqrt(dx**2 + dy**2)
     return distance
